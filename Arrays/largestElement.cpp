0568b3b46288bb59aad6ccd6554f5cf839b0ccc7
Sanjana K R <sanjanakrff186@gmail.com>
2026-04-02 00:45:02 +0530

    added first array program

---
+++ Arrays/largestElement.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+    public:
+    int largestElement( vector<int> nums)
+    {
+        int largest = nums[0];
+        for(int i=1; i< nums.size(); i++)
+        {
+            if(nums[i]>largest)
+        {
+            largest = nums[i];
+        }
+        }
+        return largest;
+    }
+};
+int main()
+{
+    Solution s1;
+    vector<int> nums = { 2,1,44,5,43,66,76,5,9};
+    int maxi = s1.largestElement(nums);
+    cout<<maxi<<endl;
+    return 0;
+
+}
